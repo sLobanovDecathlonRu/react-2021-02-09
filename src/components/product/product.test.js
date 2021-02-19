c9e823642df5173cf2fb2000193485715109519a
SLOBAN13 <sergey.lobanov@decathlon.com>
2021-02-19 13:37:47 +0300

    HT2

--- src/components/product/product.test.js
+++ src/components/product/product.test.js
@@ -22,6 +22,14 @@ describe('Product', () => {
     wrapper.find('[data-id="product-increment"]').simulate('click');
     expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
   });
+  it('should decrement amount', () => {
+    const wrapper = mount(<Product product={product} />);
+    console.log(wrapper);
+    wrapper.find('[data-id="product-increment"]').simulate('click');
+    wrapper.find('[data-id="product-increment"]').simulate('click');
+    wrapper.find('[data-id="product-decrement"]').simulate('click');
+    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
+  });
   it('should fetch data', () => {
     const fn = jest.fn();
     mount(<Product product={product} fetchData={fn} />);
--- src/components/restaurant/restaurant.js
+++ src/components/restaurant/restaurant.js
@@ -1,4 +1,5 @@
 import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
 import Menu from '../menu';
 import Reviews from '../reviews';
 import Banner from '../banner';
@@ -24,6 +25,15 @@ const Restaurant = ({ restaurant }) => {
       </div>
     </div>
   );
+};
+
+Restaurant.propTypes = {
+  restaurant: PropTypes.shape({
+    name: PropTypes.string,
+    menu: PropTypes.array,
+    id: PropTypes.number.isRequired,
+    reviews: PropTypes.array.isRequired,
+  }),
 };
 
 export default Restaurant;
